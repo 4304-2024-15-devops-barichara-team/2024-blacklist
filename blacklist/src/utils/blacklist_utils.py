1f459f016daa860b61f8ac7c3143987c4abf8b8c
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2024-10-13 11:36:01 -0500

    feat: Adding test coverage
    Signed-off-by: Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>

--- blacklist/src/utils/blacklist_utils.py
+++ blacklist/src/utils/blacklist_utils.py
@@ -5,9 +5,3 @@ from ..models.blacklist import Blacklist
 def get_blacklist_by_email(email):
     record = db.session.query(Blacklist).filter(Blacklist.email == email).first()
     return record
-
-
-def check_if_user_is_verified(user):
-    if not user.status == Status.VERIFICADO:
-        raise UserNotVerified
-    return user
