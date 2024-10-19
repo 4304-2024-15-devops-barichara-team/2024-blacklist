a1f023f66e4e2cdcf7669a44ac795eadfba05775
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2024-10-19 14:51:30 -0500

    feat: fixing health check
    Signed-off-by: Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>

--- blacklist-deployment/blueprints/health_check.py
+++ blacklist-deployment/blueprints/health_check.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-health_check_blueprint = Blueprint('health_check', __name__, url_prefix='/blacklists')
+health_check_blueprint = Blueprint('health_check', __name__, url_prefix='/')
 
 
-@health_check_blueprint.route('/ping', methods=['GET'])
+@health_check_blueprint.route('/', methods=['GET'])
 def ping_pong():
     return "pong", 200
