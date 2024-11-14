e0fc69264c1ab010ab2f354b64766470e51a6a0f
Cristian Pinzon <cc.pinzonh1@uniandes.edu.co>
2024-11-13 22:10:40 -0500

    Revert "TO BE REVERTED: Blue/Green deploy testing purposes"
    This reverts commit ec1fe1b80ab964e1c075c1771ea81bfa24ba5866.

--- blacklist/src/blueprints/health_check.py
+++ blacklist/src/blueprints/health_check.py
@@ -6,8 +6,3 @@ health_check_blueprint = Blueprint('health_check', __name__, url_prefix='/')
 @health_check_blueprint.route('/', methods=['GET'])
 def ping_pong():
     return "pong", 200
-
-
-@health_check_blueprint.route('/blue', methods=['GET'])
-def blue_green():
-    return "green", 200
