50c4477da0a63c69336b90a9779f1664f193ed27
Edgar Melara <e.melaral@uniandes.edu.co>
2024-11-16 12:10:18 -0600

    chore: provar rollback

--- blacklist/src/blueprints/health_check.py
+++ blacklist/src/blueprints/health_check.py
@@ -5,4 +5,5 @@ health_check_blueprint = Blueprint('health_check', __name__, url_prefix='/')
 
 @health_check_blueprint.route('/', methods=['GET'])
 def ping_pong():
-    return "pong", 200
+    # return "pong", 200
+    raise RuntimeError("Error intencional para probar el rollback")
